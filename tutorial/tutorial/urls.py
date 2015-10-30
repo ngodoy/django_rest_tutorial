4437acecb172a4ae9972250c49f889d5ce05fc3d
nestor <ngodoy@4geeks.com.ve>
2015-10-30 10:16:48 -0430

    Tutorial 1: Serialization

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.conf.urls import url
+from snippets import views
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('snippets.urls')),
+    #url(r'^admin/', include(admin.site.urls)),
+    #url(r'^', include('snippets.urls')),
+    url(r'^snippets/$', views.snippet_list),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 
 ]
